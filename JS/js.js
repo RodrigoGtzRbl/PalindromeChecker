362bf6cd9e2cddf444617a31b9c1aa8be3b7fb6d
RodrigoGtzRbl <Rgutierrez@protonmail.com>
2024-06-21 23:01:09 +0200

    separate " " from not palindrome

--- JS/js.js
+++ JS/js.js
@@ -11,10 +11,12 @@ $(document).ready(function () {
         str = str.replace(/ /g, '').toLowerCase();
         strReverse = str.split('').reverse().join('');
 
-        if (str == '' || str != strReverse) {
-            $('#resultContainer').html(`<h2 class="text-danger fs-4 fw-bold">"${str}" It is not a palindrome</h2>`);
+        if (str == '') {
+            $('#resultContainer').html(`<h2 class="text-warning fs-4 fw-bold">Insert a valid string</h2>`);
         } else if (str == strReverse) {
             $('#resultContainer').html(`<h2 class="text-success fs-4 fw-bold">"${str}" It is a palindrome</h2>`);
+        } else {
+            $('#resultContainer').html(`<h2 class="text-danger fs-4 fw-bold">"${str}" It is not a palindrome</h2>`);
         }
 
     }
