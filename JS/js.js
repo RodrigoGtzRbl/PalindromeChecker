328a605e1398d27746d7788b03550d6de926cdba
RodrigoGtzRbl <Rgutierrez@protonmail.com>
2024-06-21 22:59:56 +0200

    fiz null problem
    "" "" was equal as true, now its false

--- JS/js.js
+++ JS/js.js
@@ -3,17 +3,18 @@ $(document).ready(function () {
         let input = $('#input').val()
 
         checkPalindrome(input);
-        
+
     });
 
     function checkPalindrome(str) {
+
         str = str.replace(/ /g, '').toLowerCase();
         strReverse = str.split('').reverse().join('');
 
-        if (str == strReverse) {
-            $('#resultContainer').html(`<h2 class="text-success fs-4 fw-bold">"${str}" It is a palindrome</h2>`);
-        } else {
+        if (str == '' || str != strReverse) {
             $('#resultContainer').html(`<h2 class="text-danger fs-4 fw-bold">"${str}" It is not a palindrome</h2>`);
+        } else if (str == strReverse) {
+            $('#resultContainer').html(`<h2 class="text-success fs-4 fw-bold">"${str}" It is a palindrome</h2>`);
         }
 
     }
